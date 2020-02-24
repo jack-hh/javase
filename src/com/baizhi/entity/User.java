315b4d79cff9dc1f82229e2355271d66d32ea33a
hh <18510788263@163.com>
2020-02-24 20:16:30 +0800

    这是一个项目

---
+++ src/com/baizhi/entity/User.java
@@ -0,0 +1,13 @@
+package com.baizhi.entity;
+
+public class User {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
